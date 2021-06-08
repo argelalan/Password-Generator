b9001c1c61d38d2b3ff20a00cfb3bb4449a23e88
argelalan <80438622+argelalan@users.noreply.github.com>
2021-06-07 19:43:14 -0700

    Update passwords.py

--- passwords.py
+++ passwords.py
@@ -1,4 +1,3 @@
-from random_word import RandomWords
 import random
 import string
 
@@ -10,8 +9,8 @@ def generate_password():
     Print out the password.
     """
     while True:
-        valid_length = list(range(1, 95))
-        valid_length = str(valid_length)
+        valid_lengths = list(range(1, 95))
+        valid_lengths = str(valid_lengths)
 
         length = input('Enter a password length:\n(The limit is 94): ')
 
@@ -22,7 +21,7 @@ def generate_password():
 
         combination = upper + lower + num + symbols
 
-        if length in valid_length:
+        if length in valid_lengths:
             length = int(length)
 
             random_combination = random.sample(combination, length)
@@ -31,42 +30,6 @@ def generate_password():
             print(f'\nPassword:\n{password}')
             break
         elif length == 'q':
-            break
-        else:
-            print('*** Invalid syntax ***')
-
-
-def generate_passphrase():
-    """
-    Generate a passphrase formed from randomized words of the length
-    the user chooses. Separate the words with the characters chosen.
-    Print out the passphrase with the case of the user's choosing
-    """
-    length = int(input('Enter number of words in passphrase'
-                       '\n(Limit is 50 words): '))
-    spacer = input('Enter a symbol to separate the words in your passphrase'
-                   '\n(Hit space bar and enter for no symbols): ')
-
-    rw = RandomWords()
-    words = rw.get_random_words()
-
-    random_combination = random.sample(words, length)
-    passphrase = spacer.join(random_combination)
-
-    while True:
-        case = input('1. Upper\n2. Lower\n3. Title'
-                     '\nEnter a number for a case: ')
-
-        if case == '1':
-            print(f'\nPassphrase: \n{passphrase.upper()}')
-            break
-        elif case == '2':
-            print(f'\nPassphrase: \n{passphrase}')
-            break
-        elif case == '3':
-            print(f'\nPassphrase: \n{passphrase.title()}')
-            break
-        elif case == 'q':
             break
         else:
             print('*** Invalid syntax ***')
